9373e9e85a87c4fedd2ab4c90d737b45c3de8cec
Mohamdy <mohamedhamdy994@gmail.com>
2023-11-08 22:33:13 +0200

    commit

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    a_dictionary.pop(key, None)
+    if a_dictionary.get(key) is not None:
+        a_dictionary.pop(key, None)
+        return a_dictionary
