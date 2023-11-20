df4aaa786b002e824b518db70a216f9c867eb4a1
Mohamdy <mohamedhamdy994@gmail.com>
2023-11-20 19:22:31 +0200

    commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-    total = 0
-    for i in range(x):
+    num_of_elements = 0
+    for index in range(x):
         try:
-            print("{}".format(my_list[i]), end="")
-            total += 1
+            print(my_list[index], end="")
+            num_of_elements += 1
         except IndexError:
             break
-    print("")
-    return(total)
+
+    print()
+    return num_of_elements
