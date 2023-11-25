7f0d12648113e4b212ea5df1f8972119d5478536
Mohamdy <mohamedhamdy994@gmail.com>
2023-11-25 20:48:20 +0200

    commit

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import doctest
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+        raise TypeError("size must be an integer")
+    if isinstance(size, float):
+        raise TypeError("size must be an integer")
+
+    print((("#" * size + "\n") * size), end="")
+
+
+if __name__ == "__main__":
+    doctest.testfile("tests/4-print_square.txt")
