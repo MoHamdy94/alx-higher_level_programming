284f216a27faa389fa99efd391b1540a3f75cfee
Mohamdy <mohamedhamdy994@gmail.com>
2023-11-25 16:57:10 +0200

    commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Module for add_integer method"""
+
+import doctest
+
+
+def add_integer(a, b=98):
+    """Add two integers
+
+    Args:
+        a (int,float): first integer
+        b (int, float): second integer. Defaults to 98.
+    Raises:
+        TypeError: if a, b are not int, float
+    Return:
+        sum of two integer
+    """
+    if type(a) not in (int, float):
+        raise TypeError("a must be an integer")
+    if type(b) not in (int, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    doctest.testfile("tests/0-add_integer.txt")
