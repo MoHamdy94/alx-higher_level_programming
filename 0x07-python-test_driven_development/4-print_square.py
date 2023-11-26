8dbebba60962a5cc1f2c5194aba3300adefcc809
Mohamdy <mohamedhamdy994@gmail.com>
2023-11-26 23:02:18 +0200

    ommit

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -21,8 +21,3 @@ def print_square(size):
         raise TypeError("size must be an integer")
 
     print((("#" * size + "\n") * size), end="")
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/4-print_square.txt")
