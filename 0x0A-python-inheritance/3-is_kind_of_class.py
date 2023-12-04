23c285a51425e292af059f120b27a5114d13e80c
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-04 23:25:07 +0200

    is kind

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module is kind of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if an object is an instance of a given class.
+
+    Args:
+        obj: The object to be checked.
+        a_class: The class to check against.
+
+    Returns:
+        True if obj is an instance of a_class, False otherwise.
+    """
+    return isinstance(obj, a_class)
