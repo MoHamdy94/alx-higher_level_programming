0a746e4ed2840131790af7fa40a5311badf6386e
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-04 23:48:26 +0200

    empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """
+    This is the BaseGeometry class.
+
+    It is a placeholder class
+    that does not have any functionality or fields defined.
+
+    Example Usage:
+    ```python
+    # Create an instance of the BaseGeometry class
+    geometry = BaseGeometry()
+    ```
+    """
+
+    pass
