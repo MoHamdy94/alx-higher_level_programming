80979d4b0690e2e1b84f28a9d52a99aff8db5f8b
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-04 22:55:24 +0200

    commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,23 +6,10 @@ Creates a class inheriting from the list class.
 
 class MyList(list):
     """Class MyList inherits from list."""
-    class MyList(list):
-        """
-        A subclass of the built-in
-        list class in Python that adds a new method called print_sorted.
 
-        Example Usage:
-        my_list = MyList([3, 1, 2])
-        my_list.print_sorted()
+    def print_sorted(self):
+        """Prints the list, in ascending sort."""
 
-        Output:
-        [1, 2, 3]
-        """
-
-        def print_sorted(self):
-            """
-            Prints the sorted version of the list.
-            """
-            sorted_list = self[:]
-            sorted_list.sort()
-            print("{}".format(sorted_list))
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
