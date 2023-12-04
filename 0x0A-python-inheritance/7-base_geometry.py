45f2ae79b63b84b8d200c2e1503bd1315faa758c
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-05 00:06:33 +0200

    commit

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module 6-base_geometry."""
+
+
+class BaseGeometry:
+    """
+    A base class that provides a blueprint for other geometry classes.
+
+    Methods:
+    - area(): Raises an exception indicating that it is not implemented.
+    """
+
+    def area(self):
+        """
+        Placeholder method
+        for subclasses to override
+        and provide their own implementation of calculating the area.
+
+        Raises:
+        - Exception: Indicates that the method is not implemented.
+        """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """
+        Validates whether a given value
+        is an integer and whether it meets certain conditions.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Returns:
+            None: If the value is valid.
+
+        Raises:
+            False: If the name parameter is not a string.
+            TypeError: If the value parameter is not an integer.
+            ValueError: If the value parameter is less than or equal to 0.
+        """
+        if not isinstance(name, str):
+            return False
+        if not isinstance(value, int):
+            return TypeError("<name> must be an integer")
+        if value <= 0:
+            return ValueError("<name> must be greater than 0")
