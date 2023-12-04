78e71fdb05ce1f7fc0de20cf9bf190e865373944
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-05 01:09:16 +0200

    commit

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""module 100-my_int"""
+"""Module 100-my_int.
+Creates a class that inherits from int.
+"""
+
+
 class MyInt(int):
-    """
-    MyInt class that inherits from int and inverts == and != operators.
+    """Class inheriting from int,
+    But reverses the behavior of != and ==.
     """
 
     def __eq__(self, other):
-        """
-        Inverts the equality comparison.
-        """
-        return not super().__eq__(other)
+        """Equality becomes inequality."""
+
+        return super().__ne__(other)
 
     def __ne__(self, other):
-        """
-        Inverts the inequality comparison.
-        """
-        return not super().__ne__(other)
+        """Inequality becomes equality."""
+
+        return super().__eq__(other)
