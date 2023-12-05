ea79b6bc25db8f9533af95fdfb858f4d306af597
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-06 00:52:07 +0200

    commit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""Module 8-class_to_json"""
+
+
 def class_to_json(obj):
+    """
+    Returns a dictionary representation of the object's attributes.
+
+    Args:
+        obj (object):
+        The object for which
+        the dictionary representation of attributes needs to be generated.
+
+    Returns:
+        dict: A dictionary representation of the input object's attributes.
+    """
+    return obj.__dict__
