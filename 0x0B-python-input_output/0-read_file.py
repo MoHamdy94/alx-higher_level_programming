1b43e532dd836bfc78bf9642d9612f28f731f4f4
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-05 22:43:04 +0200

    commit

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """
+    Read the contents of a file and print each line.
+
+    Args:
+        filename (str, optional): The name of the file to be read. Defaults to "".
+
+    Returns:
+        None
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
