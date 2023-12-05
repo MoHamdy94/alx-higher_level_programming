321e596c23fa2934b478032686a7daa58bf8ab5d
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-05 23:15:20 +0200

    commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -13,5 +13,5 @@ def append_write(filename="", text=""):
     Returns:
         int: The number of characters written to the file.
     """
-    with open(filename, "a") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
