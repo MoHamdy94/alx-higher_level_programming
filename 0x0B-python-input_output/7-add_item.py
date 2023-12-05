ff1778b3ebd93f1b916475e51f39c086b7252a59
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-06 00:45:54 +0200

    commmit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,8 @@ import sys
 
 save_json = __import__('5-save_to_json_file').save_to_json_file
 load_json = __import__('6-load_from_json_file').load_from_json_file
+
+
 file = "add_item.json"
 try:
     new = load_json("add_item.json")
@@ -12,4 +14,4 @@ except (FileNotFoundError, ValueError):
     new = []
 for args in sys.argv[1:]:
     new.append(args)
-save_json(new, "add_item.json")
+save_json(new, file)
