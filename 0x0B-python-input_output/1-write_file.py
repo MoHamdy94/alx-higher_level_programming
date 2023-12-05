726dd0872382d5ac1cd0a925d363242be4f6985a
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-05 22:58:27 +0200

    commit

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def write_file(filename="", text=""):
+    """Write a file with the given filename
+    and content.
+    If no arguments are provided, it will create an empty file.
+    """
+    with open(filename, "w+") as f:
+        write_file = f.write(text)
+        print(write_file, end="")
