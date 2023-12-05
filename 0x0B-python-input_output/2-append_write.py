1520f1e4c65dd00dbf76b119fe98df297f95f7fd
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-05 23:16:32 +0200

    commit

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,8 +7,12 @@ def append_write(filename="", text=""):
     Appends the specified text to the given file.
 
     Args:
-        filename (str, optional): The name of the file to append the text to. Defaults to an empty string.
-        text (str, optional): The text to be appended to the file. Defaults to an empty string.
+        filename (str, optional):
+        The name of the file
+        to append the text to. Defaults to an empty string.
+        text (str, optional):
+        The text to be appended
+        to the file. Defaults to an empty string.
 
     Returns:
         int: The number of characters written to the file.
