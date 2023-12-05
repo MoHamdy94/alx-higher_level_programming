3b056b80dbfecf5c00a692a827c07b04c9d8edcf
Mohamdy <mohamedhamdy994@gmail.com>
2023-12-06 01:22:33 +0200

    commit

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,6 +5,7 @@
 class Student:
     """class of student
     """
+
     def __init__(self, first_name, last_name, age):
         """
         Initializes a new Student
@@ -25,5 +26,27 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs =None):
-        return self.__dict__
+    def to_json(self, attrs=None):
+        """
+        Convert the object attributes into a JSON-like dictionary.
+
+        Args:
+            attrs (list, optional):
+            A list of attribute names to include
+            in the JSON-like dictionary.
+            If not provided, all attributes are included.
+
+        Returns:
+            dict: A JSON-like dictionary representation
+            of the object's attributes. If `attrs` is provided,
+            only the specified attributes are included in the dictionary.
+        """
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
