ef87142013c0a1717ed0c20e4e01b2a1192adcd1
Mohamdy <mohamedhamdy994@gmail.com>
2024-01-16 22:06:26 +0200

    create database

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,3 @@
---create database if missing
-CREATE DATABASES;
+--  Creates the database 'hbtn_0c_0'
+-- If database already exists, the script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
