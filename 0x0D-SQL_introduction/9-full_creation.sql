f493fe740064e449b9f22126ded09e9a16147676
Mohamdy <mohamedhamdy994@gmail.com>
2024-01-17 00:24:33 +0200

    second table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS second_table (
     name varchar(256),
     score int
 );
-INSERT INTO second_table VALUES(1, 'john', 10);
-INSERT INTO second_table VALUES(2, 'alex', 3);
-INSERT INTO second_table VALUES(3, 'Bob', 14);
-INSERT INTO second_table VALUES(4, 'George', 8);
+INSERT INTO 'second_table' VALUES(1, 'john', 10);
+INSERT INTO 'second_table' VALUES(2, 'alex', 3);
+INSERT INTO 'second_table' VALUES(3, 'Bob', 14);
+INSERT INTO 'second_table' VALUES(4, 'George', 8);
