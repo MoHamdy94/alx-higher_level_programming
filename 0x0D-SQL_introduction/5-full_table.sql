1673ee7262b0423a60f09d7ebb846a616f4e4fb2
Mohamdy <mohamedhamdy994@gmail.com>
2024-01-16 23:39:00 +0200

    describe

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
---prints the full description of the table
---first_table from the database hbtn_0c_0 in your MySQL server.
-DESCRIBE hbtn_0c_0;
+-- prints the full description of a table first_table
+SHOW CREATE TABLE first_table;
