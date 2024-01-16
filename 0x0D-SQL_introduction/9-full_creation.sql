18007af759ccc09560e17928380da1533c6ca09d
Mohamdy <mohamedhamdy994@gmail.com>
2024-01-17 00:26:03 +0200

    commit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,7 @@
 -- Create table 'second_table' in db 'hbtn_0c_0'
 -- Addprops  (id INT), (name VARCHAR(256)), (score INT)
 CREATE TABLE IF NOT EXISTS second_table (id int,name varchar(256),score int);
+-- Script should create 4 records
 INSERT INTO 'second_table' VALUES(1, 'john', 10);
 INSERT INTO 'second_table' VALUES(2, 'alex', 3);
 INSERT INTO 'second_table' VALUES(3, 'Bob', 14);
