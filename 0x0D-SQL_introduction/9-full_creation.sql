8226ad29f4e55ab0600eb4721339b4f7e4cc38e3
Mohamdy <mohamedhamdy994@gmail.com>
2024-01-17 00:22:29 +0200

    second table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- Create table 'second_table' in db 'hbtn_0c_0'
+-- Addprops  (id INT), (name VARCHAR(256)), (score INT)
+CREATE TABLE second_table (
+    id int,
+    name varchar(256),
+    score int
+);
+INSERT INTO second_table VALUES(1, 'john', 10);
+INSERT INTO second_table VALUES(2, 'alex', 3);
+INSERT INTO second_table VALUES(3, 'Bob', 14);
+INSERT INTO second_table VALUES(4, 'George', 8);
