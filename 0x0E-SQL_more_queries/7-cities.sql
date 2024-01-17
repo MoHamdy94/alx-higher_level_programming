e1c4d82296b34b2079ef1dbb3b152c0cebb913e6
Mohamdy <mohamedhamdy994@gmail.com>
2024-01-18 01:17:41 +0200

    cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,18 +1,14 @@
--- Create a new database named 'hbtn_0d_usa' if it doesn't exist
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Create a new table named 'cities' if it doesn't exist
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS cities (
-    -- Assign a unique ID for each city record
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-
-    -- Assign a state ID for each city record to establish a relationship with the 'states' table
-    state_id INT NOT NULL,
-
-    -- Store the name of the city
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id),
-
-    -- Establish a foreign key constraint between 'state_id' and 'id' column of 'states' table
-    FOREIGN KEY (state_id) REFERENCES states(id)
+  id INT NOT NULL AUTO_INCREMENT,
+  state_id INT NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (state_id) REFERENCES states(id)
 );
