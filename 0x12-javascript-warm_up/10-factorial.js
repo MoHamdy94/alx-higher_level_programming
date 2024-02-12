7edd28f1c135deedece69aebe67453f0dee47aa3
MoHamdy94 <mohamedhamdy994@gmail.com>
2024-02-12 23:34:21 +0200

    commit

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-const num = process.argv[2];
-let result = 1;
-for (let i = 1; i <= num; i++) {
-  result *= i;
+function factorial (num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  console.log(result);
 }
-console.log(result);
+factorial(process.argv[2]);
