2118167520e1c7643b787cc88b8eaa39a3d59a7a
MoHamdy94 <mohamedhamdy994@gmail.com>
2024-02-12 22:45:01 +0200

    COMMIT

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,8 +4,6 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 } else {
   const number = Number(process.argv[2]);
   for (let i = 0; i < number; i++) {
-    // for (let j = 0; j < number; j++) {
-    // }
     console.log('x'.repeat(number));
   }
 }
