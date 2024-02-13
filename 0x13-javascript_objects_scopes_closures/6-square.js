cabec775b6d5c04361fc8f90cced98b2dea1c6a7
Mohamdy <mohamedhamdy994@gmail.com>
2024-02-13 19:43:09 +0200

    commit

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,7 +8,13 @@ class Square extends SquareModel {
   }
 
   charPrint (c) {
-    this.print(c);
+    if (c == null) {
+        c = "X"
+    }
+    for (let i = 0; i < this.width; i++) {
+        console.log(c.repeat(this.width));
+    }
+    
   }
 }
 module.exports = Square;
