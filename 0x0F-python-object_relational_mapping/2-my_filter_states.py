2f26193af9c38d009540117508fce0794e70c3d3
Mohamdy <mohamedhamdy994@gmail.com>
2024-02-18 01:42:12 +0200

    commit

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""  display all table in a state where name matched the arg """
+"""a script that lists all states start with N from the database"""
+
 import MySQLdb
 import sys
-
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-              .format(sys.argv[4]))
-    rows = c.fetchall()
-    for row in rows:
-        print(row)
-    c.close()
-    db.close()
+    db = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursr = db.cursor()
+    stateName = sys.argv[4]
+    cursr.execute("SELECT * FROM states WHERE name = '{}' \
+        ORDER BY id ASC;".format(stateName))
+    states = cursr.fetchall()
+    for state in states:
+        print(state)
