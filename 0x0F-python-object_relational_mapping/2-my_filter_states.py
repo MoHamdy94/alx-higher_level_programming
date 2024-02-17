11162ad0ad69490ec9ddf1290984b05bae0d2243
Mohamdy <mohamedhamdy994@gmail.com>
2024-02-18 01:38:45 +0200

    commitr

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     c = db.cursor()
     stateName = sys.argv[4]
-    c.execute("""SELECT * FROM states WHERE name = '{}' \
+    c.execute("""SELECT * FROM states WHERE name = '{}' 
             ORDER BY id ASC;""".format(stateName))
     rows = c.fetchall()
     for row in rows:
