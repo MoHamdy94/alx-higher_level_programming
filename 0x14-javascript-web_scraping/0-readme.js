35513d7f214a13e43eda966bdb7a56a8100f031e
Mohamdy <mohamedhamdy994@gmail.com>
2024-03-26 16:47:11 +0200

    task0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -6,7 +6,6 @@ fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
-    console.log(data.toString());  
+    console.log(data.toString());
   }
-  
 });
