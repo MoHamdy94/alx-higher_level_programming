305398c29c605d0d1d5913dec6b1b9e1f7af9789
Mohamdy <mohamedhamdy994@gmail.com>
2024-03-26 17:04:34 +0200

    task2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+
+// Get the URL from the first command-line argument
+const url = process.argv[2];
+
+// Send the GET request using request
+request(url, function(err, response)  {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
